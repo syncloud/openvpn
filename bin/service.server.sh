90563c7761e123061dfdae80b0acab90fe0630a4
Boris Rybalkin <ribalkin@gmail.com>
2019-12-20 23:32:05 +0000

    fix nat cleanup

--- bin/service.server.sh
+++ bin/service.server.sh
@@ -19,7 +19,7 @@ start)
       mknod /dev/net/tun c 10 200
     fi
     echo "1" > /proc/sys/net/ipv4/ip_forward
-    iptables -t nat -D POSTROUTING 1
+    iptables -t nat -D POSTROUTING 1 || true
     iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
     while [ ! -f ${SERVER_CONF} ]
     do
