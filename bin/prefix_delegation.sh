00fd658207858cc9afd85d2b485f01b960bd0ab3
Boris Rybalkin <ribalkin@gmail.com>
2020-04-23 22:43:54 +0100

    convert to 64

--- bin/prefix_delegation.sh
+++ bin/prefix_delegation.sh
@@ -9,12 +9,15 @@ case $reason in
             if [ "$new_ip6_prefix" != "$old_ip6_prefix" ]; then
                 # enable on new prefix
                 logger -t "openvpn-ipv6" "enable ipv6: $new_ip6_prefix"
-                sed -i 's@.*server-ipv6.*@server-ipv6 '$new_ip6_prefix'@g' ${SERVER_CONF}
+                ip6_no_mask=$(echo $new_ip6_prefix | cut -f1 -d'/')
+                ip6="$ip6_no_mask/64"
+                logger -t "openvpn-ipv6" "enable ipv6: $ip6"
+                sed -i 's@.*server-ipv6.*@server-ipv6 '$ip6'@g' ${SERVER_CONF}
                 snap restart openvpn
             fi
         else
             # disable ipv6
-            logger -t "openvpn-ipv6" "disable ipv6: $new_ip6_prefix"
+            logger -t "openvpn-ipv6" "disable ipv6"
             sed -i 's@.*server-ipv6.*@#server-ipv6@g' ${SERVER_CONF}
             snap restart openvpn
         fi
