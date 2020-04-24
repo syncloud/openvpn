f461c243e2c02ad598257d02c28e68fc983d217c
Boris Rybalkin <ribalkin@gnail.com>
2020-04-24 22:39:26 +0100

    disable previously enabled ip6 prefixes only

--- bin/prefix_delegation.sh
+++ bin/prefix_delegation.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
 SERVER_CONF=/var/snap/openvpn/current/openvpn/server.conf
+CURRENT_IPV6_PREFIX_FILE=/var/snap/openvpn/current/ipv6_prefix
 
 case $reason in
     BOUND6|EXPIRE6|REBIND6|REBOOT6|RENEW6)
         logger -t "openvpn-ipv6" "event reason: $reason, new_ip6_prefix: $new_ip6_prefix, old_ip6_prefix: $old_ip6_prefix"
+        current_ip6_prefix=""
+        if [ -f $CURRENT_IPV6_PREFIX_FILE ]; then
+            current_ip6_prefix=$(cat $CURRENT_IPV6_PREFIX_FILE)
+        fi
         if [ -n "$new_ip6_prefix" ]; then
-            if [ "$new_ip6_prefix" != "$old_ip6_prefix" ]; then
+            if [ "$new_ip6_prefix" != "$current_ip6_prefix" ]; then
                 # enable on new prefix
                 ip6_no_prefix=$(echo $new_ip6_prefix | cut -f1 -d'/')
                 prefix=$(echo $new_ip6_prefix | cut -f2 -d'/')
@@ -23,10 +28,12 @@ case $reason in
                 snap restart openvpn
             fi
         else
-            # disable ipv6
-            logger -t "openvpn-ipv6" "disable ipv6"
-            sed -i 's@.*server-ipv6.*@#server-ipv6@g' ${SERVER_CONF}
-            snap restart openvpn
+            if [ "$old_ip6_prefix" == "$current_ip6_prefix" ]; then
+                # disable ipv6
+                logger -t "openvpn-ipv6" "disable ipv6"
+                sed -i 's@.*server-ipv6.*@#server-ipv6@g' ${SERVER_CONF}
+                snap restart openvpn
+            fi
         fi
         ;;
 esac
