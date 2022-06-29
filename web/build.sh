1d9e3e5c0f7eed8efdb03dbe0a1edb364f0ae74a
Boris Rybalkin <ribalkin@gmail.com>
2022-06-29 22:07:26 +0100

    web

--- web/build.sh
+++ web/build.sh
@@ -3,7 +3,6 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
-BUILD_DIR=${DIR}/build/snap
-cd ${DIR}/web
+BUILD_DIR=${DIR}/../build/snap
 mkdir ${BUILD_DOR}/web
 go build -o ${BUILD_DIR}/web/openvpn-web-ui 
