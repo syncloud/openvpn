48fa9da04cd89bd42db092e8dfe4db36155feb59
Boris Rybalkin <ribalkin@gmail.com>
2022-07-02 10:50:04 +0100

    fix gen keys

--- bin/generate-keys.sh
+++ bin/generate-keys.sh
@@ -6,9 +6,9 @@ CA_NAME=LocalCA
 SERVER_NAME=server
 export KEY_NAME=$CA_NAME
 echo "Generating CA cert"
-$EASY_RSA/easyrsa --batch build-ca nopass
+/snap/openvpn/current/easy-rsa/easyrsa --batch build-ca nopass
 
 export KEY_NAME=$SERVER_NAME
 echo "Generating server cert"
-$EASY_RSA/build-key-server $SERVER_NAME
-$EASY_RSA/easyrsa --batch build-server-full $SERVER_NAME nopass
+/snap/openvpn/current/easy-rsa/build-key-server $SERVER_NAME
+/snap/openvpn/current/easy-rsa/easyrsa --batch build-server-full $SERVER_NAME nopass
