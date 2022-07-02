a67c528294b6592bac5207171f14e940d330822c
Boris Rybalkin <ribalkin@gmail.com>
2022-07-02 13:01:49 +0100

    init pki

--- bin/generate-keys.sh
+++ bin/generate-keys.sh
@@ -4,10 +4,10 @@ CA_NAME=LocalCA
 SERVER_NAME=server
 export KEY_NAME=$CA_NAME
 echo "Generating CA cert"
+/snap/openvpn/current/easy-rsa/easyrsa init-pki
 /snap/openvpn/current/easy-rsa/easyrsa --batch build-ca nopass
 
 export KEY_NAME=$SERVER_NAME
 echo "Generating server cert"
 /snap/openvpn/current/easy-rsa/build-key-server $SERVER_NAME
 /snap/openvpn/current/easy-rsa/easyrsa --batch build-server-full $SERVER_NAME nopass
-h build-server-full $SERVER_NAME nopass
