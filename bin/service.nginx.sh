79da2feb255556b029690499068834070879bf5d
Boris Rybalkin <ribalkin@gmail.com>
2023-12-06 20:18:34 +0000

    config

--- bin/service.nginx.sh
+++ bin/service.nginx.sh
@@ -4,4 +4,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
 /bin/rm -f ${SNAP_COMMON}/web.socket
 /bin/rm -f ${SNAP_COMMON}/log/nginx*.log
-exec ${DIR}/nginx/sbin/nginx -c ${SNAP_COMMON}/config/nginx/nginx.conf -p ${DIR}/nginx -e stderr
+exec ${DIR}/nginx/sbin/nginx -c ${SNAP_DATA}/config/nginx/nginx.conf -p ${DIR}/nginx -e stderr
