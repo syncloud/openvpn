9ecaf577ed86050c8bfea5a7a3b870ef0a662cc6
Boris Rybalkin <ribalkin@gmail.com>
2025-11-20 20:23:13 +0000

    libcap-ng-dev

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ PREFIX=${DIR}/build/snap/openvpn
 NAME=openvpn
 
 apt update
-apt -y install liblzo2-dev libpam-dev net-tools libnl-genl-3-dev
+apt -y install liblzo2-dev libpam-dev net-tools libnl-genl-3-dev libcap-ng-dev
 
 cd ${DIR}/build/openvpn-*
 ./configure --prefix=${PREFIX}
