727cabfd9b519906b0f056ff66b2dba6fceb4abf
Boris Rybalkin <ribalkin@gmail.com>
2025-11-21 07:35:57 +0000

    fix test

--- openvpn/test.sh
+++ openvpn/test.sh
@@ -5,4 +5,5 @@ cd ${DIR}
 
 BUILD_DIR=${DIR}/../build/snap/openvpn
 ls -la ${BUILD_DIR}/lib
-${BUILD_DIR}/sbin/openvpn.sh --help
+${BUILD_DIR}/sbin/openvpn.sh --help || true
+${BUILD_DIR}/sbin/openvpn.sh --version
