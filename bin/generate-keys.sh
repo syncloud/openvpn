1012a84931bba51ff6c1043b7ab41cda9fd3037b
Boris Rybalkin <ribalkin@gmail.com>
2025-11-22 08:43:55 +0000

    fix conf

--- bin/generate-keys.sh
+++ bin/generate-keys.sh
@@ -2,7 +2,7 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
 EASYRSA_BIN=$DIR/easyrsa/easyrsa
-VARS_FILE=$DIR/easyrsa/vars
+VARS_FILE=$DIR/config/easyrsa/vars
 
 "$EASYRSA_BIN" --vars="$VARS_FILE" --batch build-ca nopass | logger
 "$EASYRSA_BIN" --vars="$VARS_FILE" --batch build-server-full server nopass | logger
