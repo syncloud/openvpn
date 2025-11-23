372b582ce415d0f725bbddf713db1312120341c1
Boris Rybalkin <ribalkin@gmail.com>
2025-11-23 18:13:30 +0000

    logger

--- bin/service.web.sh
+++ bin/service.web.sh
@@ -2,7 +2,7 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
-export CONFIG_DIR=${SNAP_DATA}/config/web
-echo "${CONFIG_DIR}" | logger -t openvpn-web
+export CONFIG_DIR=/var/snap/openvon/current/config/web
+echo "${CONFIG_DIR}"
 cd $DIR/web
-exec ./openvpn-web-ui --config=${CONFIG_DIR}  | logger -t openvpn-web
+exec ./openvpn-web-ui --config=${CONFIG_DIR}
