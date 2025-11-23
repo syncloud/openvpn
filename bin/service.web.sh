05785a96ab467c534afd33bbc47a2b1f2d7acc59
Boris Rybalkin <ribalkin@gmail.com>
2025-11-23 21:32:23 +0000

    config

--- bin/service.web.sh
+++ bin/service.web.sh
@@ -2,7 +2,7 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
-export CONFIG_DIR=/var/snap/openvon/current/config/web
+export CONFIG_DIR=/var/snap/openvpn/current/config/web
 echo "${CONFIG_DIR}"
 cd $DIR/web
 exec ./openvpn-web-ui --config=${CONFIG_DIR}
