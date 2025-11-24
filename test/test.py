5dddd32dc3ea5826a2f4042ba2c173dcd8b7d138
Boris Rybalkin <ribalkin@gmail.com>
2025-11-24 18:51:30 +0000

    nft ruoes test

--- test/test.py
+++ test/test.py
@@ -58,16 +58,16 @@ def test_index(app_domain):
     assert response.status_code == 200, response.text
 
 
-def test_rules(device):
-    assert 1 == int(device.run_ssh('nft list chain ip nat POSTROUTING| grep masquerade | wc -l'))
-
-
 def test_prefix_delegation(device):
     device.run_ssh('/snap/openvpn/current/bin/prefix_delegation.sh', env_vars='reason=BOUND6 new_ip6_prefix=111:222:333::/64')
 
 
 def test_upgrade(app_archive_path, device_host, device_password):
     local_install(device_host, device_password, app_archive_path)
+
+
+def test_rules(device):
+    assert 1 == int(device.run_ssh('nft list chain ip nat POSTROUTING| grep masquerade | wc -l'))
 
 
 def test_remove(device, app):
